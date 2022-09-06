a228cde3bbed2a511beea9cb131ac1b89e5acbd8
Jacob Chvatal <jake@isnt.online>
2022-09-05 23:56:44 -0400

    new vite new me

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.js',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
-  }
+  plugins: [preact()]
 })
